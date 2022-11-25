ed5e6c66f81199d46943775faacce3739a62110e
Maruf <01780527702maruf@gmail.com>
2022-11-25 19:57:11 +0600

    data get

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
 const express = require('express')
-const app = express()
+const app = express('cors')
 const port = process.env.PORT || 5000;
+const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
+app.use(cors())
+app.use(express.json())
+
+//resaledb
+//9jm39RWsv3uWHHFK
+
+
+
+const uri = "mongodb+srv://resaledb:9jm39RWsv3uWHHFK@cluster0.ei8vvcb.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const alltypeCollection = client.db('Resale').collection('all categories');
+        app.get('/categories', async (req, res) => {
+            const query = {};
+            const categories = await alltypeCollection.find(query).toArray();
+            res.send(categories)
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(error => console.error(error))
+
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('server is running')
 })
 
 app.listen(port, () => {
