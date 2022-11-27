8c4663c2eb5b964f06c250cb27f3d879f359fa3e
Maruf <01780527702maruf@gmail.com>
2022-11-27 14:24:43 +0600

    env added

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json())
 
 
 
-const uri = "mongodb+srv://resaledb:9jm39RWsv3uWHHFK@cluster0.ei8vvcb.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ei8vvcb.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
